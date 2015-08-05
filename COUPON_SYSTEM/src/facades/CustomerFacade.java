f57d9191cb7db60542b303127058f6e11b66119d
alex <cjmade@yahoo.com>
2015-08-05 18:50:26 +0300

    -כותב פה מה עדכנת-
    -מסמן פה את הקבצים שרוצה להעלות-

---
+++ COUPON_SYSTEM/src/facades/CustomerFacade.java
@@ -0,0 +1,16 @@
+package facades;
+
+import exceptions.InvalidLoginException;
+
+public class CustomerFacade implements ClientFacade
+{
+
+	@Override
+	public ClientFacade login(String name, String password, String clientType)
+			throws InvalidLoginException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
